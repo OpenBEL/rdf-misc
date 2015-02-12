2812c7c060615ea18610e3661d9f1c31f276f392
Anthony Bargnesi <abargnesi@gmail.com>
2015-02-12 12:32:10 -0500

    broke sqlite3 into three separate steps
    1. Load RDF file into sqlite database (using redlander).
    2. Export annotation and namespace concepts to JSON; prepped for
       FTS indexing (e.g. includes a globbed "text" field)
    3. Build FTS tables; import JSON concepts.

---
+++ create-librdf-db/build-fts-table.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'sqlite3'
+require 'multi_json'
+
+# defaults
+options = {
+  name: 'rdf.db',
+  debug: false,
+  input_file: 'concepts.json'
+}
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: build-fts-table.rb -n DATABASE_NAME -i FILE"
+  opts.on("-n", "--name NAME", "Storage name     (defaults to #{options[:name]})") do |name|
+    options[:name] = name
+  end
+  opts.on("-i", "--input-file FILE", "Input file to read concept JSON from.    (defaults to #{options[:input_file]})") do |input_file|
+    options[:input_file] = input_file
+  end
+  opts.on("-d", "--debug", "Debug?           (defaults to #{options[:debug]})") do
+    options[:debug] = true
+  end
+end
+parser.parse!
+
+if not File.readable?(options[:input_file])
+  $stderr.puts "The #{options[:input_file]} file is not readable.\n"
+  exit 1
+end
+
+def symbolize_keys!(hash)
+  hash.keys.each do |key|
+    hash[key.to_sym] = hash.delete(key)
+  end
+  hash
+end
+
+db = SQLite3::Database.new options[:name]
+jf = File.new(options[:input_file], "r")
+db.execute('''PRAGMA journal_mode = OFF''')
+db.execute('''PRAGMA synchronous = OFF''')
+db.execute('''
+  CREATE VIRTUAL TABLE
+    concepts_fts
+  USING
+    fts4(uri, concept_type, scheme_uri, identifier, pref_label, title, text, tokenize=unicode61 "tokenchars=,-()\'")
+''')
+fts_db_stmt = db.prepare(
+  '''insert into
+       concepts_fts(uri, concept_type, scheme_uri, identifier, pref_label, title, text)
+     values
+       (:uri, :concept_type, :inScheme, :identifier, :prefLabel, :title, :text)'''
+)
+begin
+  i = 0
+  jf.each do |line|
+    concept = symbolize_keys!(MultiJson.load(line))
+    fts_db_stmt.execute(
+      :uri => concept[:uri],
+      :concept_type => concept[:concept_type],
+      :inScheme     => concept[:inScheme].first,
+      :identifier   => concept[:identifier],
+      :prefLabel    => concept[:prefLabel],
+      :title        => concept[:title],
+      :text         => concept[:text]
+    )
+
+    i+=1
+    if options[:debug] && i % 5000 == 0
+      puts "#{i} concepts fts-indexed"
+    end
+  end
+ensure
+  fts_db_stmt.close
+  db.execute('''INSERT INTO concepts_fts(concepts_fts) VALUES(\'optimize\')''');
+  db.close
+end
+# vim: ts=2 sw=2
---
+++ create-librdf-db/export-concepts.rb
@@ -0,0 +1,108 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'redlander'
+require 'uri'
+require 'multi_json'
+
+# defaults
+options = {
+  new: 'no',
+  name: 'rdf.db',
+  debug: false,
+  output_file: 'concepts.json'
+}
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: export-concepts.rb -n DATABASE_NAME -o FILE"
+  opts.on("-n", "--name NAME", "Storage name     (defaults to #{options[:name]})") do |name|
+    options[:name] = name
+  end
+  opts.on("-o", "--output-file FILE", "Output file to write concept JSON to.    (defaults to #{options[:output_file]})") do |output_file|
+    options[:output_file] = output_file
+  end
+  opts.on("-d", "--debug", "Debug?           (defaults to off)") do
+    options[:debug] = true
+  end
+end
+
+parser.parse!
+
+AN_CONCEPT = 'http://www.openbel.org/vocabulary/AnnotationConcept'
+NS_CONCEPT = 'http://www.openbel.org/vocabulary/NamespaceConcept'
+RDF_TYPE   = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+
+# describe the uri and collect [predicate,object] pairs to a hash
+def describe_concept(concept_uri, concept_type, rdf_model)
+  concept = Hash.new { |hash, key| hash[key] = [] }
+  concept[:uri] = concept_uri
+  concept[:concept_type] = concept_type
+  rdf_model.statements.each(
+    :subject => Redlander::Node.new(concept_uri[1...-1], :resource => true),
+    :predicate => nil,
+    :object => nil
+  ) do |rdf_s|
+    predicate = rdf_s.predicate.to_s
+    object    = rdf_s.object.respond_to?(:value) ? rdf_s.object.value.to_s : rdf_s.object.to_s
+
+    index = predicate.rindex('#') || predicate.rindex('/')
+    if index
+      attribute = predicate[index+1...-1]
+      concept[attribute.to_sym] << object
+    end
+  end
+
+  # derive :text value
+  [ :type, :identifier, :prefLabel, :title, :altLabel ].each do |key|
+    concept[key] = concept[key].join(' ').squeeze(' ').strip()
+  end
+  id  = concept[:identifier]
+  lbl = concept[:prefLabel]
+  ttl = concept[:title]
+  alt = concept[:altLabel]
+  concept[:text] = "#{id} #{lbl} #{ttl} #{alt}".squeeze(' ').strip()
+
+  concept
+end
+
+def map_concepts(rdf_model, concept_type, concept_type_uri)
+  rdf_model.statements.each(
+    :subject => nil,
+    :predicate => Redlander::Node.new(RDF_TYPE, :resource => true),
+    :object => Redlander::Node.new(concept_type_uri, :resource => true)
+  ).lazy.map { |rdf_s|
+    describe_concept(rdf_s.subject.to_s, concept_type, rdf_model)
+  }
+end
+
+output_file = File.new(options[:output_file], File::CREAT|File::TRUNC|File::RDWR, 0644)
+begin
+  rdf_model = Redlander::Model.new(name: options[:name], storage: 'sqlite', synchronous: 'off')
+  if options[:debug]
+    puts "Mapping concepts to JSON for annotation concepts"
+  end
+  i = 0
+  map_concepts(rdf_model, :annotation_concept, AN_CONCEPT).each do |concept|
+    output_file << (MultiJson.dump(concept) + "\n")
+    i += 1
+    if i % 5000 == 0
+      output_file.flush
+      puts "#{i} concepts fts-indexed" if options[:debug]
+    end
+  end
+
+  if options[:debug]
+    puts "Mapping concepts to JSON for namespace concepts"
+  end
+  i = 0
+  map_concepts(rdf_model, :namespace_concept, NS_CONCEPT).each do |concept|
+    output_file << (MultiJson.dump(concept) + "\n")
+    i += 1
+    if i % 5000 == 0
+      output_file.flush
+      puts "#{i} concepts fts-indexed" if options[:debug]
+    end
+  end
+ensure
+  output_file.close
+end
+# vim: ts=2 sw=2
